a986e664f016de957177d9089c1e5e48480fc864
tanju <tanju.github@server.fake>
2013-07-14 17:42:18 +0200

    Initial file and first test with git and github

---
+++ arduino_photoctrl.ino
@@ -0,0 +1,19 @@
+/************************************************************************************
+ * arduino photoctrl
+ *
+ * photo control application for arduino
+ *
+ * https://github.com/tanju/arduino_photoctrl
+ *
+ * Jan Arnhold, July 2013
+ *
+ ************************************************************************************/
+ 
+ void setup()
+ {
+ }
+ 
+ void loop()
+ {
+ }
+ 
